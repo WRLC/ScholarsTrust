0ab0abb6da064d5ffdf60c3bcf5c64bfba01b36d
Tom Boone <boone@wrlc.org>
2019-09-09 11:22:02 -0400

    Add production URLs to sites.php

--- web/sites/sites.php
+++ web/sites/sites.php
@@ -1,3 +1,5 @@
 <?php
 
 $sites['ra8.lndo.site'] = 'rosemont.wrlc.org';
+$sites['rosemontsharedprintalliance.org']     = 'rosemont.wrlc.org';
+$sites['www.rosemontsharedprintalliance.org'] = 'rosemont.wrlc.org';
