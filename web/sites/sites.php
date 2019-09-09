834de8c899b60b449e291a72da398c91c7af6764
Tom Boone <boone@wrlc.org>
2019-09-09 12:41:42 -0400

    Add dev site to sites.php

--- web/sites/sites.php
+++ web/sites/sites.php
@@ -3,3 +3,4 @@
 $sites['ra8.lndo.site'] = 'rosemont.wrlc.org';
 $sites['rosemontsharedprintalliance.org']     = 'rosemont.wrlc.org';
 $sites['www.rosemontsharedprintalliance.org'] = 'rosemont.wrlc.org';
+$sites['ra8.wrlc.org'] = 'rosemont.wrlc.org';
